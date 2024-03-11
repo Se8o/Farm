e92dd9b0b22c1cac62499d8a8f56bc59b9936d96
lodin <lodin.sebastian6@gmail.com>
2024-03-11 08:40:51 +0100

    přidání metody na zvíšení šance na růst po zavlažení přidání konstruktoru a getterů a setterů

--- src/Flower.java
+++ src/Flower.java
@@ -5,6 +5,46 @@ public abstract class Flower {
     protected double neededArea;
     protected double chanceOfGrowth;
 
+    public Flower(String name, double price, double neededArea, double chanceOfGrowth) {
+        this.name = name;
+        this.price = price;
+        this.neededArea = neededArea;
+        this.chanceOfGrowth = chanceOfGrowth;
+    }
+// region getters and setters
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public double getNeededArea() {
+        return neededArea;
+    }
+
+    public void setNeededArea(double neededArea) {
+        this.neededArea = neededArea;
+    }
+
+    public double getChanceOfGrowth() {
+        return chanceOfGrowth;
+    }
+
+    public void setChanceOfGrowth(double chanceOfGrowth) {
+        this.chanceOfGrowth = chanceOfGrowth;
+    }
+    //endregion
+
     @Override
     public String toString() {
         return "Flower{" +
---
+++ src/Tulipan.java
@@ -0,0 +1,7 @@
+public class Tulipan extends Flower{
+    private String color;
+    public Tulipan(String name, double price, double neededArea, double chanceOfGrowth){
+        super(name, price, neededArea, chanceOfGrowth);
+        this.color = color;
+    }
+}
